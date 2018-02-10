76543c51099b50ba3437e58ad9dbe31b6f998194
yaodongcai <1207542617@qq.com>
2018-02-10 21:14:53 +0800

    add publish course pro

--- 初练音乐小程序/lib/teacherReserveClass.js
+++ 初练音乐小程序/lib/teacherReserveClass.js
@@ -6,7 +6,29 @@ class TeacherReserve {
   constructor() {
     // 构造函数
   }
+  // 发布课程
+  publishCourse(img_url, userid, data) {
+    // 声明类型
+    var Todo = AV.Object.extend('phoneTeacherPublishCourse');
+    // 新建对象
+    var item = new Todo();
+    wx.showLoading({
+      title: '正在提交',
+      mask: true
+    });
 
+    // 设置名称
+    item.set('userid', userid);
+    // 设置优先级
+    item.set('img_url', img_url);
+    item.set('name', data.name);
+    item.set('time', data.time);
+    item.set('address', data.address);
+    item.set('description', data.description);
+    item.set('cost', data.cost);
+    var promise = item.save();
+    return promise;
+  }
   reserveTeacherByUserName(
     teacherID,// 老师的信息
     userPhone,//用户输入的电话号码
--- 初练音乐小程序/pages/publish/publish.js
+++ 初练音乐小程序/pages/publish/publish.js
@@ -1,5 +1,7 @@
 //logs.js
 const util = require('../../utils/util.js')
+var TeacherPublishCourse = require('../../lib/teacherReserveClass.js');
+var app = getApp(); // 获取当前的app
 var File = require('../../lib/File.js');
 Page({
   data: {
@@ -11,8 +13,44 @@ Page({
   },
   // 发布这个课程
   publishCourse: function(e) {
-        console.log( 'img_url' + JSON.stringify(e.detail))
-        console.log( 'name' + e.detail.value.name)
+       
+        console.log( 'name' + e.detail.value.name);
+        console.log('img' + this.data.logo);
+        var user = app.globalData.user;
+        var img_url = '';
+        var course = {
+          name:'',
+          cost:'',
+          time:'',
+          description:'',
+          address:''
+        };
+
+        course.name = e.detail.value.name;
+        course.cost = e.detail.value.cost;
+        course.time = e.detail.value.time;
+        course.description = e.detail.value.description;
+        course.address = e.detail.value.address;
+        var self = this;
+        var teacherPublishCourse = new TeacherPublishCourse();
+        teacherPublishCourse.publishCourse(
+          self.data.logo,
+          user.username,
+          course
+        ).then(function (res) {
+          // 提示用户发送成功：
+          wx.hideLoading();
+          wx.showToast({
+            title: '预约成功',
+            icon: 'success',
+            duration: 2000
+          });
+        }, function (error) {
+
+        })
+        // 将数据存储到对应的表中
+ 
+       
   },
   chooseImageTap: function () {
     let _this = this;
@@ -38,20 +76,20 @@ Page({
       sizeType: ['original', 'compressed'],
       sourceType: [type],
       success: function (res) {
-        console.log(res);
         var tempFilePath = res.tempFilePaths[0];
           _this.setData({
             logo: tempFilePath
           })
 
-        // var promise = (new File()).uploadImageAndGetUrl(tempFilePath);
-        // promise.then(function(res2) {
-        //   _this.setData({
-        //     logo: res2
-        //   })
-        // }, function(error) {
+          var promise = (new File()).uploadImageAndGetUrl(_this.data.logo);
+          promise.then(function (file) {
+            console.log(JSON.stringify(file))
+            _this.setData({
+              logo: file
+            })
+          }, function (error) {
 
-        // } )
+          })
         
       }
     })
